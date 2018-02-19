4c8ceb3a61a2f8ebf95986de172a972294121eb9
Luis Daniel Pambid <36612330+frosteen@users.noreply.github.com>
2018-02-19 13:24:36 +0800

    Update PRACTICE1.py

--- PRACTICE1.py
+++ PRACTICE1.py
@@ -1,7 +1,6 @@
 nintendoPrice=50000
-print 'The price of Nintnedo Wii is Php.',nintendoPrice
+print 'The price of Nintendo Wii is Php.',nintendoPrice
 money=input("Input your amount of money: ")
 print 'The amount of money you have is Php.',money
 print 'The number of Nintendo Wiis you can buy is',money/nintendoPrice
 print 'The amount you need to afford another Nintendo Wii is Php.',nintendoPrice-money%nintendoPrice
-input()
